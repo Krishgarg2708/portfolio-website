72b07b30fa356aaa131ea3887e601afe89b9d600
Krishgarg2708 <krish.garg2708@gmail.com>
2025-11-04 20:28:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Portfolio Website Loaded Successfully 🚀");
+// Dark/Light Mode Toggle
+const toggleBtn = document.getElementById("modeToggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  const icon = toggleBtn.querySelector("i");
+  icon.classList.toggle("fa-sun");
+  icon.classList.toggle("fa-moon");
+});
