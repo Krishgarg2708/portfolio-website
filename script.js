2d308c1d323c6d6e4b5e0f4dcf928a4913f7eb11
Krishgarg2708 <krish.garg2708@gmail.com>
2025-11-05 09:48:48 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Dark/Light Mode Toggle
-const toggleBtn = document.getElementById("modeToggle");
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  const icon = toggleBtn.querySelector("i");
-  icon.classList.toggle("fa-sun");
-  icon.classList.toggle("fa-moon");
-});
